610509e38e894ab019ace8b864f0da1d2c03b2b7
Your Name <you@example.com>
2024-10-28 18:59:09 +0530

    first commit

---
+++ bc.js
@@ -0,0 +1,10 @@
+let a=document.getElementById('bill')
+let b=document.getElementById('given')
+let c=document.getElementById('btn')
+let f=document.getElementById('p')
+c.addEventListener('click',function(){
+ 
+   let d=b.value-a.value
+    f.append(d)
+    f.innerHTML=`Balance Amount ${d}`
+})
